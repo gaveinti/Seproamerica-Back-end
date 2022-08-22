7a32f4d2c35612ab5c46805f628ad87f01c35af8
eacorsab <emascord@espol.edu.ec>
2022-08-22 15:13:52 -0500

    base de datos agregada

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seproamericaDB1',
+        'USER': 'root',
+        'PASSWORD': 'centro234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
