c07155115d4bbcb50dec4d208d1fbe63ca650020
gaveinti <gaveinti>
2022-08-23 17:33:58 -0500

    intento de base de datos

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, url
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
-    url(r'^', include('clienteWA.urls')),
+    #re_path(r'^', include('clienteWA.urls')),
 ]
