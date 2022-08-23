669cbdcafe8dbf4805b70a4e0b69eb1509b7162b
gaveinti <gaveinti>
2022-08-23 15:58:58 -0500

    editando views.py de clientewa

--- clienteWA/views.py
+++ clienteWA/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from .serializers import FileSerializer
 
 # Create your views here.
