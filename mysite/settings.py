f15eb039467d4bd71dbeb6980ce5cd0e40eff976
gaveinti <gaveinti>
2022-08-23 22:38:42 -0500

    contraseña para base de datos

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'seproamericaDB',
         'USER': 'root',
-        'PASSWORD': 'emelec',
+        'PASSWORD': 'centro234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
