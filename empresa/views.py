4d36e71876db93549199f7ab6da56103feca9ac7
gaveinti <gaveinti>
2022-09-02 21:13:42 -0500

    Configuracion para obtener usuario por medio de cedula en db

--- empresa/views.py
+++ empresa/views.py
@@ -25,3 +25,21 @@ def usuarioRegistro(request):
             usuarioARegistrar_Serializer.save()
             return JsonResponse(usuarioARegistrar_Serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(usuarioARegistrar_Serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#Función para obtener datos de un usuario para validacion en inicio sesion
+@api_view(['GET'])
+@csrf_exempt
+def usuarioInicioSesion(request, pk, *args):
+    #pk = self.kwargs.get('pk')
+    #Encontrar usuario por pk (correo)
+    print(usuario.objects.get())
+
+    try:
+        usuarioAEncontrar = usuario.objects.get(pk = pk)
+    except usuario.DoesNotExist:
+        return JsonResponse({'message' : 'El usuario no existe'}, status=status.HTTP_404_NOT_FOUND)
+
+    #mandar datos para validación
+    if request.method == 'GET':
+        usuarioAEncontrar_serializer = UsuarioSerializer(usuarioAEncontrar) #El del final es el modelo
+        return JsonResponse(usuarioAEncontrar_serializer.data)
--- mysite/settings.py
+++ mysite/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 # Next, set CORS_ORIGIN_ALLOW_ALL and add the host to CORS_ORIGIN_WHITELIST:
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:55719',
+    'http://localhost:4200',
 )
 
 ROOT_URLCONF = 'mysite.urls'
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'seproamericaDB',
         'USER': 'root',
-        'PASSWORD': 'centro1234',
+        'PASSWORD': 'emelec',
         'HOST': 'localhost',
         'PORT': '3306',
     }
