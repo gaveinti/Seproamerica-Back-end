8381f15046b0ae3f6e8e9d450b2c91d16851fa66
Robinson Flores <rjflores@espol.edu.ec>
2022-11-20 21:23:43 -0500

    serializar modelos de prsonal

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import usuario,rol
+from .models import usuario,rol,personalOperativo,personalAdministrativo
 
 admin.site.register(usuario)
 admin.site.register(rol)
+admin.site.register(personalAdministrativo)
+admin.site.register(personalOperativo)
--- empresa/serializers.py
+++ empresa/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers 
-from empresa.models import usuario 
- 
+from empresa.models import usuario, personalAdministrativo, personalOperativo 
 class UsuarioSerializer(serializers.ModelSerializer):
  
     class Meta:
@@ -17,3 +16,39 @@ class UsuarioSerializer(serializers.ModelSerializer):
                   'contrasenia',
                   'fechaRegistro',
                   'rol')
+
+class PersonalAdministrativoSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = personalAdministrativo
+        fields = ('idPersonal',
+                  'inicioOperanciones',
+                  'finOpeaciones',
+                  'sucursal',
+                  'cedula',
+                  'cargo',
+                  'estado',
+                  'fechaModificacion',
+                  #'rol' 
+                  )
+
+class PersonalOperativoSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = personalOperativo
+        fields = ('sexo_CHOICES',
+                  'idPersonal',
+                  'numCedula',
+                  'apellidos',
+                  'nombres',
+                  'fechaNac',
+                  'sexo',
+                  'direccion',
+                  'telefono',
+                  'correo',
+                  'fechaRegistro',
+                  'agregadoPor',
+                  'sucursal',
+                  'estado',
+                  #'rol'
+                  )
