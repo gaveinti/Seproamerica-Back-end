1ac2b731040d445e18e84b8037ccb52ef61d6c1a
gaveinti <gaveinti>
2023-01-28 19:56:49 -0500

    Corregido de actualizacion de datos y validaciones de tabla

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,14 +123,11 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'seproamerica2022$seproamericadb',
-        'USER': 'seproamerica2022',
-        'PASSWORD': 'Admin2022',
-        'HOST': 'seproamerica2022.mysql.pythonanywhere-services.com',
-        #'PORT': '3306',
-        'OPTIONS':{
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8mb4',
-        },
+        'NAME': 'seproamericadb',
+        'USER': 'root',
+        'PASSWORD': 'emelec',
+        'HOST': 'localhost',
+        'PORT': '3306',
 
     }
 }
